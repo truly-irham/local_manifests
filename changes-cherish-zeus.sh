2197ed54d807e8abfb40e8a3c1743895ec05d0d9
truly-irham <ep0107566@student.uniten.edu.my>
2024-04-29 21:36:22 +0800

    Initial CherishOS bringup for zeus

--- changes-cherish-zeus.sh
+++ changes-cherish-zeus.sh
@@ -2,22 +2,9 @@
 set -e
 
 source "build/envsetup.sh";
-source "vendor/derp/build/envsetup.sh";
+source "vendor/cherish/build/envsetup.sh";
 
 # Reference/Source: https://xdaforums.com/t/development-of-lineageos-and-oss-kernel-for-xiaomi-sm-gen-4-5-10-devices.4589247/
-
-# frameworks/base
-changes=(
-386158 # Add 5G Ultra Wideband icon carrier config keys
-386159 # Fix default values for 5G Ultra Wideband icon carrier config keys
-)
-repopick -g 'https://review.lineageos.org' -P frameworks/base ${changes[@]}&
-
-# vendor/derp
-changes=(
-367044 # android: merge_dtbs: Respect miboard-id while merging
-)
-repopick -g 'https://review.lineageos.org' -P vendor/derp ${changes[@]}&
 
 # device/qcom/sepolicy_vndr/sm8450
 changes=(
@@ -65,19 +52,4 @@ git revert 93250170f8ad3a29ac62a1a0f83649af4b8dcd00
 # Revert "input: touchsreen: xiaomi: Import updated header from M11"
 git revert 2ecbc6be1670837b29cb7b12717e3a7e00415f99
 
-cd ../../..
-
-# Revert "[Wi-Fi] Remove 'Do not validate' option in CA certificate spinner"
-cd packages/modules/Wifi
-git remote show matrixx >/dev/null 2>&1 && echo "Remote 'matrixx' already exists." || git remote add matrixx https://github.com/ProjectMatrixx/android_packages_modules_Wifi.git
-git fetch matrixx
-git cherry-pick 833cd86
-cd ../../..
-
-cd packages/apps/Settings
-git remote show matrixx >/dev/null 2>&1 && echo "Remote 'matrixx' already exists." || git remote add matrixx https://github.com/ProjectMatrixx/android_packages_apps_Settings.git
-git fetch matrixx 
-git cherry-pick 1204b94
-git rm res/values/cr_strings.xml
-git cherry-pick --continue
 cd ../../..
