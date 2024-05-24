d64fdf5c3597d8ff43a89ea8b013061f09418c38
truly-irham <ep0107566@student.uniten.edu.my>
2024-05-25 01:21:08 +0800

    Include vibrator related changes
    Source: https://xdaforums.com/t/development-of-lineageos-and-oss-kernel-for-xiaomi-sm-gen-4-5-10-devices.4589247/

--- changes-derp-zeus.sh
+++ changes-derp-zeus.sh
@@ -27,11 +27,22 @@ changes=(
 352661 # sensors: Make sensor set mode operation function virtual
 352662 # sensors: Move one shot sensor out of main class
 352663 # sensors: Fix locking around setOperationMode and activate
-352664 # sensors: Add udfps long press sensor
+352664 # sensors: Create sysfs polling one shot sensor
+392967 # sensors: Let the reading of poll fd be configurable
+392968 # sensors: Add SysfsPollingOneShotSensor constructor without enable path
+392969 # sensors: Add udfps long press sensor using SysfsPollingOneShotSensor
 352665 # sensors: Handle fod_pressed_state without coordinates
 363160 # hidl: biometrics: fingerprint: Add enroll methods to udfps handler
+392965 # vibrator: effect: Create double click effect from click if necessary
+392966 # vibrator: effect: Fallback to click if an effect is missing
 )
 repopick -g 'https://review.lineageos.org' -P hardware/xiaomi ${changes[@]}&
+
+# vendor/qcom/opensource/vibrator
+changes=(
+392952 # vibrator: Use a better check for primitive effects
+)
+repopick -g 'https://review.lineageos.org' -P vendor/qcom/opensource/vibrator ${changes[@]}&
 
 wait
 
