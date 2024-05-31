cfa6415b052091945703c5288c6daaca7a0cc7a3
truly-irham <ep0107566@student.uniten.edu.my>
2024-05-31 23:34:30 +0800

    Track hardware/xiaomi with required changes

--- changes-lineage-zeus.sh
+++ changes-lineage-zeus.sh
@@ -4,26 +4,6 @@ set -e
 source "build/envsetup.sh";
 source "vendor/lineage/build/envsetup.sh";
 
-# hardware/xiaomi
-changes=(
-352657 # Add dummy sensors sub HAL
-352658 # sensors: Make sensor batch function virtual
-352659 # sensors: Make sensor run function virtual
-352660 # sensors: Make sensor flush function virtual
-352661 # sensors: Make sensor set mode operation function virtual
-352662 # sensors: Move one shot sensor out of main class
-352663 # sensors: Fix locking around setOperationMode and activate
-352664 # sensors: Create sysfs polling one shot sensor
-392967 # sensors: Let the reading of poll fd be configurable
-392968 # sensors: Add SysfsPollingOneShotSensor constructor without enable path
-392969 # sensors: Add udfps long press sensor using SysfsPollingOneShotSensor
-352665 # sensors: Handle fod_pressed_state without coordinates
-363160 # hidl: biometrics: fingerprint: Add enroll methods to udfps handler
-392965 # vibrator: effect: Create double click effect from click if necessary
-392966 # vibrator: effect: Fallback to click if an effect is missing
-)
-repopick -f -P hardware/xiaomi ${changes[@]}&
-
 # frameworks/base
 changes=(
 386158 # Add 5G Ultra Wideband icon carrier config keys
