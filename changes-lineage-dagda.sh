97ab203d693fe0a896a636a2a4c3cb86e4527e6f
truly-irham <ep0107566@student.uniten.edu.my>
2024-06-19 00:46:55 +0800

    Import hardware/xiaomi from LineageOS with updated repopicks script

--- changes-lineage-dagda.sh
+++ changes-lineage-dagda.sh
@@ -4,6 +4,8 @@ set -e
 source "build/envsetup.sh";
 source "vendor/lineage/build/envsetup.sh";
 
+# Source: https://gist.github.com/Gelbpunkt/699551aebc5ae12c821ad2ea18b21409
+
 # frameworks/base
 changes=(
 386158 # Add 5G Ultra Wideband icon carrier config keys
@@ -16,6 +18,25 @@ changes=(
 367044 # android: merge_dtbs: Respect miboard-id while merging
 )
 repopick -g 'https://review.lineageos.org' -f -P vendor/lineage ${changes[@]}&
+
+# hardware/xiaomi
+changes=(
+352657 # Add dummy sensors sub HAL
+352658 # sensors: Make sensor batch function virtual
+352659 # sensors: Make sensor run function virtual
+352660 # sensors: Make sensor flush function virtual
+352661 # sensors: Make sensor set mode operation function virtual
+352662 # sensors: Move one shot sensor out of main class
+352663 # sensors: Fix locking around setOperationMode and activate
+352664 # sensors: Create sysfs polling one shot sensor
+392967 # sensors: Let the reading of poll fd be configurable
+392969 # sensors: sensors: Implement udfps long press sensor
+393396 # sensors: Implement single tap sensor
+393397 # sensors: Implement double tap sensor
+392965 # vibrator: effect: Create double click effect from click if necessary
+392966 # vibrator: effect: Fallback to click if an effect is missing
+)
+repopick -g 'https://review.lineageos.org' -P hardware/xiaomi ${changes[@]}&
 
 wait
 
