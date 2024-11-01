4d451fec295554bb6226e967e1bd8bc1f9e46f87
truly-irham <ep0107566@student.uniten.edu.my>
2024-11-02 01:10:17 +0800

    Update changes for LineageOS 22

--- changes-lineage-dagda.sh
+++ changes-lineage-dagda.sh
@@ -4,39 +4,22 @@ set -e
 source "build/envsetup.sh";
 source "vendor/lineage/build/envsetup.sh";
 
-# Source: https://gist.github.com/Gelbpunkt/699551aebc5ae12c821ad2ea18b21409
+# Source: https://gitlab.com/cupid-development/automated-builds/-/raw/main/repopicks.sh
 
-# frameworks/base
+# hardware/qcom-caf/sm8450/audio/primary-hal
 changes=(
-386158 # Add 5G Ultra Wideband icon carrier config keys
-386159 # Fix default values for 5G Ultra Wideband icon carrier config keys
+406470 # configs: Allow setsockopt syscall for qcom c2audio
+406471 # hal: Guard primary audio HAL in case target provides prebuilt
+406472 # hal: Move audio primary HAL guard
+406473 # hal: Depend on agmserver and palserver headers
 )
-repopick -g 'https://review.lineageos.org' -f -P frameworks/base ${changes[@]}&
+repopick -g https://review.lineageos.org -P hardware/qcom-caf/sm8450/audio/primary-hal ${changes[@]}&
 
-# vendor/lineage
+# hardware/qcom-caf/sm8450/display
 changes=(
-367044 # android: merge_dtbs: Respect miboard-id while merging
+397275 # gralloc: Allow devices to opt-in for YCrCb camera video encode
 )
-repopick -g 'https://review.lineageos.org' -f -P vendor/lineage ${changes[@]}&
-
-# hardware/xiaomi
-changes=(
-352657 # Add dummy sensors sub HAL
-352658 # sensors: Make sensor batch function virtual
-352659 # sensors: Make sensor run function virtual
-352660 # sensors: Make sensor flush function virtual
-352661 # sensors: Make sensor set mode operation function virtual
-352662 # sensors: Move one shot sensor out of main class
-352663 # sensors: Fix locking around setOperationMode and activate
-352664 # sensors: Create sysfs polling one shot sensor
-392967 # sensors: Let the reading of poll fd be configurable
-392969 # sensors: sensors: Implement udfps long press sensor
-393396 # sensors: Implement single tap sensor
-393397 # sensors: Implement double tap sensor
-392965 # vibrator: effect: Create double click effect from click if necessary
-392966 # vibrator: effect: Fallback to click if an effect is missing
-)
-repopick -g 'https://review.lineageos.org' -P hardware/xiaomi ${changes[@]}&
+repopick -g https://review.lineageos.org -P hardware/qcom-caf/sm8450/display ${changes[@]}&
 
 wait
 
